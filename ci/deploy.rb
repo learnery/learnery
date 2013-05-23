9aaae5ce894d2aac3d19402b5c5849fe0508aed2
Dr Blinken <drblinken@gmail.com>
2013-05-23 12:36:00 +0200

    deploy: adapted Gemfile check to actual travis values

--- ci/deploy.rb
+++ ci/deploy.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 require 'heroku-headless'
-puts "ENV[\"BUNDLE_GEMFILE\"]"
-puts ENV["BUNDLE_GEMFILE"]
-puts ENV["BUNDLE_GEMFILE"].inspect
-gf = ENV["BUNDLE_GEMFILE"]
-if gf == "Gemfile" || gf == "" || gf == nil
+
+unless /learnery\/Gemfile$/.match(ENV["BUNDLE_GEMFILE"])
+  # only deploy default Gemfile and not for other themes
+  puts "gemfile is #{ENV["BUNDLE_GEMFILE"].inspect} - skipping deploy"
+else
   HerokuHeadless.configure do | config |
     config.post_deploy_commands = [
       'rake db:migrate'
