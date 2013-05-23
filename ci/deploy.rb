fcf0ab95eb5a11beea307136a81496efa48113d6
Dr Blinken <drblinken@gmail.com>
2013-05-23 12:11:42 +0200

    heroku deploy: added config

--- ci/deploy.rb
+++ ci/deploy.rb
@@ -1,4 +1,7 @@
 require 'heroku-headless'
 HerokuHeadless.configure do | config |
+  config.post_deploy_commands = [
+    'rake db:migrate'
+  ]
 end
 HerokuHeadless::Deployer.deploy( 'learnery-staging' )
