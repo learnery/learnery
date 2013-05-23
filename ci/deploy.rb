799c270d23d37bec7ac3d0e67fb8eb5b9f5a76b5
Dr Blinken <drblinken@gmail.com>
2013-05-23 12:23:29 +0200

    added Gemfile check to ci/deploy

--- ci/deploy.rb
+++ ci/deploy.rb
@@ -1,7 +1,14 @@
+#!/usr/bin/env ruby
 require 'heroku-headless'
-HerokuHeadless.configure do | config |
-  config.post_deploy_commands = [
-    'rake db:migrate'
-  ]
+puts "ENV[\"BUNDLE_GEMFILE\"]"
+puts ENV["BUNDLE_GEMFILE"]
+puts ENV["BUNDLE_GEMFILE"].inspect
+gf = ENV["BUNDLE_GEMFILE"]
+if gf == "Gemfile" || gf == "" || gf == nil
+  HerokuHeadless.configure do | config |
+    config.post_deploy_commands = [
+      'rake db:migrate'
+    ]
+  end
+  HerokuHeadless::Deployer.deploy( 'learnery-staging' )
 end
-HerokuHeadless::Deployer.deploy( 'learnery-staging' )
