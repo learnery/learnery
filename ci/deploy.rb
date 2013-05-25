054758051cf0e47ebee4cada46c693db65ab09af
Dr Blinken <drblinken@gmail.com>
2013-05-25 21:49:53 +0200

    changes from headless deploy

--- ci/deploy.rb
+++ ci/deploy.rb
@@ -23,15 +23,15 @@ else
   end
 
   puts "deploying to heroku app #{app_name}"
-
+  remote_name = "headlessheroku"
   HerokuHeadless.configure do | config |
     config.post_deploy_commands = ['rake db:migrate']
     config.pre_deploy_git_commands = [
       "git remote -v",
       "git commit -am  \"changes from headless deploy\" ",
-      "git remote add heroku git@heroku.com:#{app_name}.git",
-      "git fetch heroku",
-      "git merge -m \"merged by automatic deploy\" heroku/master"]
+      "git remote add #{remote_name} git@heroku.com:#{app_name}.git",
+      "git fetch #{remote_name}",
+      "git merge -m \"merged by automatic deploy\" #{remote_name}/master"]
   end
 
   result = HerokuHeadless::Deployer.deploy( app_name )
