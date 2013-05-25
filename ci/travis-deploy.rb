07a34aa7be76335be8bf93d87f9f857078a42548
Dr Blinken <drblinken@gmail.com>
2013-05-25 18:03:42 +0200

    working on deployment

--- ci/travis-deploy.rb
+++ ci/travis-deploy.rb
@@ -2,12 +2,19 @@
 # this is called by travis ci
 require 'heroku-headless'
 
+%w(HEROKU_API_KEY LEARNERY_THEME TRAVIS_TEST_RESULT)
+
+#export HEROKU_API_KEY=<secret>
+#export LEARNERY_THEME=default
+#export TRAVIS_TEST_RESULT="0"
+
 if ENV['TRAVIS_TEST_RESULT'] == "0"
 
-  if 'default' == ENV['LEARNERY_THEME']
-    app_name = "learnery-staging"
-  else
-    app_name = "learnery-staging-#{ENV['LEARNERY_THEME']}"
+  app_name = ARGV[0] ? ARGV[0] : 'learnery-staging'
+  puts app_name
+
+  unless 'default' == ENV['LEARNERY_THEME']
+    app_name = "#{app-name}-#{ENV['LEARNERY_THEME']}"
   end
 
   puts "deploying to heroku app #{app_name}"
@@ -15,7 +22,6 @@ if ENV['TRAVIS_TEST_RESULT'] == "0"
   HerokuHeadless.configure do | config |
     config.post_deploy_commands = ['rake db:migrate']
   end
-
-  HerokuHeadless::Deployer.deploy( app_name )
-
+  result = HerokuHeadless::Deployer.deploy( app_name )
+  exit result ? 0 : 1
 end
