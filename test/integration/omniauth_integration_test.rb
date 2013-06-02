0b4ef5969b91414cdd1e23ca7c0cd52c106265b4
B Kleinen <drblinken@gmail.com>
2013-06-02 19:41:32 +0200

    integration test for twitter and github oauth

--- test/integration/omniauth_integration_test.rb
+++ test/integration/omniauth_integration_test.rb
@@ -2,19 +2,11 @@ require "test_helper"
 
 describe "omniauth integration test" do
   before do
-    puts "****************************"
-    puts self.class
     OmniAuth.config.test_mode = true
- #   request.env["devise.mapping"] = Devise.mappings[:user]
- #   request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
   end
-#  it "logs in user with twitter" do
-#    visit root_path
-#    click_link t(:sign_up_with, :provider => :twitter)
-#    page.must_have_content t(:logged_in_as, :user => user.email)
-#  end
 
   describe "handles callbacks from providers" do
+
     context "twitter" do
       before do
         OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
@@ -31,5 +23,23 @@ describe "omniauth integration test" do
         page.must_have_content t(:logged_in_as, :user => user.user_info)
       end
     end
+
+    context "github" do
+      before do
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+             :provider => 'github',
+             :uid => '123545',
+             :info => {:nickname => 'TheTwitterer' }
+        })
+      end
+
+      it "logs in user with github" do
+        visit root_path
+        click_link t(:login_with, provider: :twitter)
+        user = User.where(nickname: 'TheTwitterer').first
+        page.must_have_content t(:logged_in_as, :user => user.user_info)
+      end
+    end
+
   end
 end
