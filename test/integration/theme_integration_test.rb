3d20c9e7da66016fef6b94ef0e2c86bb632204bf
Dr Blinken <drblinken@gmail.com>
2013-06-24 22:31:05 +0200

    moved test tmp files to tmp

--- test/integration/theme_integration_test.rb
+++ test/integration/theme_integration_test.rb
@@ -1,26 +1,32 @@
 require "test_helper"
 require 'RMagick'
 
+SCREENSHOT_DIR = 'tmp'
+SCREENSHOT_FILENAME = File.join(SCREENSHOT_DIR,'screenshot.png')
+OUT_FILENAME = File.join(SCREENSHOT_DIR,'out.png')
+COMPARE_FILENAME = File.join(SCREENSHOT_DIR,'compare.png')
+
 describe "Theme Integration Test" do
   setup do
     Capybara.current_driver = Capybara.javascript_driver # :selenium by default
   end
 
   it "may take a screenshot" do
-    screenshot_file_name = ENV['LEARNERY_THEME']? ENV['LEARNERY_THEME'] : "default"
-    screenshot_fixture_file  = "test/fixtures/screenshots/#{screenshot_file_name}.png"
+
+    FIXTURE_FILENAME = ENV['LEARNERY_THEME']? ENV['LEARNERY_THEME'] : "default"
+    screenshot_fixture_file  = "test/fixtures/screenshots/#{FIXTURE_FILENAME}.png"
     screenshot_fixture_image = Magick::Image::read(screenshot_fixture_file).first
 
     visit root_path
-    save_screenshot('screenshot.png')
-    image = Magick::Image.read('screenshot.png').first
+    save_screenshot(SCREENSHOT_FILENAME)
+    image = Magick::Image.read(SCREENSHOT_FILENAME).first
     image_right_size = image.resize_to_fill(screenshot_fixture_image.columns, screenshot_fixture_image.rows, Magick::NorthWestGravity)
-    image_right_size.write('out.png')
+    image_right_size.write(OUT_FILENAME)
 
     ilg = Magick::ImageList.new
     ilg.push( screenshot_fixture_image )
     ilg.push( image_right_size )
-    ilg.append(true).write("compare.png")
+    ilg.append(true).write(COMPARE_FILENAME)
 
     errors= image_right_size.difference( screenshot_fixture_image )
     assert_in_delta( errors[1], 0, 0.1 )
