b416c0878b3ee07c3dc09acf3b2c1c13c4de085f
Dr Blinken <drblinken@gmail.com>
2013-06-29 12:09:27 +0200

    engine: db migration

--- db/migrate/20130531171605_defaults_for_events.rb
+++
@@ -1,9 +0,0 @@
-class DefaultsForEvents < ActiveRecord::Migration
-  def up
-    execute "UPDATE events SET description = ' ' WHERE description IS NULL"
-    change_column :events, :description, :string, :default => "", :null => false
-  end
-  def down
-    change_column :events, :description, :string, :default => nil, :null => true
-  end
-end
--- db/migrate/20130601150236_add_username_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration
-  def change
-    remove_index(:users, :name => 'index_users_on_email', :column =>  :email)
-    add_column :users, :username, :string
-    add_index :users, [:email, :username], :unique => true
-    add_index :users, :email
-    add_index :users, :username
-  end
-end
