6e9144a4bbc62c4a94b3418438eadbe41b9f2057
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-07-21 12:47:23 +0200

    cleand up all the helpers

--- app/helpers/learnery/application_helper.rb
+++ app/helpers/learnery/application_helper.rb
@@ -1,19 +1,11 @@
 module Learnery
-module ApplicationHelper
-  include Learnery::FormHelper
-  #include Learnery::AuthenticationHelper
-  include BootstrapFlashHelper
-  include Learnery::RsvpHelper
+  module ApplicationHelper
+    include Learnery::FormHelper
+    #include Learnery::AuthenticationHelper
+    include BootstrapFlashHelper
+    include Learnery::RsvpHelper
 
 
 
-  # for events
-
-  protected
-  def admin_only
-    #raise "admin only"
-    redirect_to root_path, :notice => t(:admin_only)  unless current_user and current_user.admin?
   end
-end
-
 end
